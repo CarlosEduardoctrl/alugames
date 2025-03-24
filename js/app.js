5d3bf6dbbf675773b32d7f26b51558f9cc5cd890
Carlos Eduardo <caduu10fera@gmail.com>
2025-03-24 17:58:54 -0300

    confirmação devolução

--- js/app.js
+++ js/app.js
@@ -1,16 +1,18 @@
-function alterarStatus(id){
-    let gameClicado = document.getElementById (`game-${id}`);
+function alterarStatus(id) {
+    let gameClicado = document.getElementById(`game-${id}`);
     let imagem = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
     let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
 
-if(imagem.classList.contains('dashboard__item__img--rented')) {
-    imagem.classList.remove('dashboard__item__img--rented');
-    botao.textContent = 'Alugar'
-    botao.classList.remove('dashboard__item__button--return')
-}else {
-    imagem.classList.add('dashboard__item__img--rented');
-    botao.textContent = 'Devolver'
-    botao.classList.add('dashboard__item__button--return')
-}
+    if(imagem.classList.contains('dashboard__item__img--rented')) {
+        if (confirm('Deseja realmente devolver o jogo?')) {
+            imagem.classList.remove('dashboard__item__img--rented');
+            botao.textContent = 'Alugar';
+            botao.classList.remove('dashboard__item__button--return');
+        }
+    } else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.textContent = 'Devolver';
+        botao.classList.add('dashboard__item__button--return');
+    }
 }
