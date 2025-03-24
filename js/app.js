800a9f5be4faff46d63cdb10e2db109447f26c1b
Carlos Eduardo <caduu10fera@gmail.com>
2025-03-24 18:06:19 -0300

    console quant. alugados

--- js/app.js
+++ js/app.js
@@ -1,18 +1,26 @@
+let jogosAlugados = 1;
+
+function exibirJogosAlugados(){
+console.log(`total de jogos alugados ${jogosAlugados}`);
+}
+
 function alterarStatus(id) {
     let gameClicado = document.getElementById(`game-${id}`);
     let imagem = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
-    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
 
     if(imagem.classList.contains('dashboard__item__img--rented')) {
         if (confirm('Deseja realmente devolver o jogo?')) {
             imagem.classList.remove('dashboard__item__img--rented');
             botao.textContent = 'Alugar';
             botao.classList.remove('dashboard__item__button--return');
+            jogosAlugados--;
         }
     } else {
         imagem.classList.add('dashboard__item__img--rented');
         botao.textContent = 'Devolver';
         botao.classList.add('dashboard__item__button--return');
+        jogosAlugados++;
     }
+    exibirJogosAlugados();
 }
